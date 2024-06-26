8f731ecf540e039962224922f0afee35b44c9b16
Tom H <tom.h@mailosaur.com>
2024-06-26 17:14:22 +0100

    Added hyperlink colors

--- src/inputs/serverIdInput.ts
+++ src/inputs/serverIdInput.ts
@@ -1,4 +1,5 @@
 import prompts from 'prompts';
+import pc from 'picocolors';
 
 import { exit } from '@/helpers';
 
@@ -9,7 +10,9 @@ export async function serverIdInput(): Promise<string> {
     {
       type: 'text',
       name: 'value',
-      message: 'Enter your Server ID (see https://mailosaur.com/app/servers):',
+      message: `Enter your Server ID (see ${pc.blue(
+        'https://mailosaur.com/app/servers'
+      )}):`,
       validate: async (value: string) => {
         if (value.length === 8) {
           return true;
